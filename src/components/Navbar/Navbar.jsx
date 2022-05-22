a67a0c493527b104e3d5aa4cfd68ba83f3f2a839
badouine <nzbadouine@gmail.com>
2022-05-22 02:12:23 +0000

    navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { GiHamburgerMenu } from 'react-icons/gi';
 import { MdOutlineRestaurantMenu } from 'react-icons/md';
 
-import images from '../../constants/images'
+import images from '../../constants/images';
 import './Navbar.css';
 
 const Navbar = () => (
@@ -17,6 +17,23 @@ const Navbar = () => (
        <li className="p__opensans"><a href="#awards">Awards</a></li>
        <li className="p__opensans"><a href="#contact">Contact</a></li>
      </ul>
+
+     <div className="app__navbar-login">
+      <a href="#login" className="p__opensans">Log In / Register</a>
+     </div>
+     <a href="/" className="p__opensans">Book Table</a>
+     <div className="app__navbar-smallscreen">
+       <GiHamburgerMenu color="#fff" fontSize={27} onClick={() => {}}/>
+       <div className="app__navbar-smallscreen_overlay flex_center slide_bottom">
+       <ul className="app__navbar-smallscreen-links">
+       <li className="p__opensans"><a href="#home">Home</a></li>
+       <li className="p__opensans"><a href="#about">About</a></li>
+       <li className="p__opensans"><a href="#menu">Menu</a></li>
+       <li className="p__opensans"><a href="#awards">Awards</a></li>
+       <li className="p__opensans"><a href="#contact">Contact</a></li>
+     </ul>
+     </div>
+     </div>
   </nav>
 );
 
